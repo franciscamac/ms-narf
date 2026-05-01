f0369bb1defcda74478a7f4dfb46e913bc27d5a0
franciscamac <franpms89@gmail.com>
2026-05-01 17:05:19 -0300

    Project hr-payroll created

--- hr-worker/src/main/java/com/franciscamaca/hr_worker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/franciscamaca/hr_worker/repositories/WorkerRepository.java
@@ -1,2 +1,9 @@
-package com.franciscamaca.hr_worker.repositories;public class WorkerRepository {
+package com.franciscamaca.hr_worker.repositories;
+
+
+import com.franciscamaca.hr_worker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
 }
